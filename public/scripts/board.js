bbd39be99de084625077a76f5fe2a5a79baae1ac
ethanvoss <74939378+ethanvoss@users.noreply.github.com>
2020-12-23 14:01:41 -0800

    Update board.js

--- public/scripts/board.js
+++ public/scripts/board.js
@@ -175,7 +175,7 @@ class Board
 		else if (document.styleSheets[1].rules)
 			myCss = document.styleSheets[1].rules
 		else return;
-		console.log(myCss[0]);
+		console.log(myCss);
 		myCss[myCss.length-1].style.backgroundColor = darkColor;
 		myCss[myCss.length-2].style.backgroundColor = lightColor;
 	}
