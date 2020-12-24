3e69e0dbb5aa1a5805d046a576f61905e76dae44
ethanvoss <74939378+ethanvoss@users.noreply.github.com>
2020-12-24 11:45:00 -0800

    Update trainers.js

--- routes/trainers.js
+++ routes/trainers.js
@@ -7,7 +7,36 @@ if(sideline) sideline picker =>
 trainer
 */
 router.get('/', checkAuthenticated, async (req, res) => {
-	res.render('trainers/index');
+	const lines = await Line.find({userId: req.user._id}).lean();
+	const lineIds = [];
+	const lineNames = [];
+	const startingPositions = [];
+	const lockedLines = [];
+	const lockedLineParents = [];
+	lines.forEach((line) => {
+		if(line.transposable === false)
+		{
+			lockedLines.push(line._id);
+			lockedLineParents.push(line.parentLine);	
+		}
+		lineIds.push(line._id);
+		lineNames.push(line.name);
+		startingPositions.push(line.startingPosition);
+	})
+	var initObj = {
+		lineIds: lineIds, 
+		lineNames: lineNames, 
+		startingPositions: startingPositions, 
+		lockedLines: lockedLines, 
+		lockedLineParents: lockedLineParents
+	};
+	if(req.query.current)
+	{
+		initObj.current = req.query.current;
+		const positions = await Line.findOne({_id: req.query.current}, 'positions').lean();
+		initObj.positions = positions.positions; //this is a really stupid quirk of mongoose
+	}
+	res.render('trainers/index', initObj);
 });
 
 router.get('/sideline', checkAuthenticated, async (req, res) => {
