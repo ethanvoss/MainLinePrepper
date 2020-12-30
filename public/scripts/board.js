81090a62061a748f30ebfd1248a5480bece794e3
ethanvoss <74939378+ethanvoss@users.noreply.github.com>
2020-12-29 21:44:12 -0800

    commit

--- public/scripts/board.js
+++ public/scripts/board.js
@@ -1,3 +1,5 @@
+const { Chess } = require('./chess.js')
+var chess;
 const pieces = ['K','Q','R','B','N','P'];
 const collumConvert = ['a','b','c','d','e','f','g','h'];
 const rowConvert = ['8','7','6','5','4','3','2','1'];
@@ -12,6 +14,7 @@ class Board
 		this.size = initObj.size || 8;
 		this.locked = initObj.locked || false;
 		this.startingFen = initObj.startingFen || 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';
+		ches = new Chess(this.startingFen);
 		const boardId = initObj.boardId || 'board';
 		this.board = document.getElementById(boardId);
 		this.position = [];
