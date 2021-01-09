e863e11fccc5450763ca743f10397711e2de8de1
ethanvoss <74939378+ethanvoss@users.noreply.github.com>
2021-01-09 06:35:07 -0800

    'commit'

--- routes/vossbots.js
+++ routes/vossbots.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Chess } = require('./chess.js')
+const { Chess } = require('chess.js')
 const chess = new Chess()
 
 router.get('/', checkAuthenticated, (req, res) => {
