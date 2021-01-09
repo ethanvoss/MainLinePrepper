0c0ed43494a28b0f16530cf1368fab1097668c8e
ethanvoss <74939378+ethanvoss@users.noreply.github.com>
2021-01-09 07:27:26 -0800

    'commit'

--- routes/vossbots.js
+++ routes/vossbots.js
@@ -37,8 +37,8 @@ router.get('/getmove', (req, res) => {
 		moves.forEach((currentMove) => {
 			if(currentMove.eval > highest.eval) highest = currentMove;
 		});
-		console.log(`sending ${highest}`);
-		res.send(highest);
+		console.log(`sending ${JSON.stringify(highest)}`);
+		res.send(highest.move);
 	} else {
 		res.send('Bee bee boo beep i cant find a move');
 	}	
