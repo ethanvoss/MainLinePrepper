7cc5c942dbe6ac666bf5690878e338206c4a57ed
ethanvoss <74939378+ethanvoss@users.noreply.github.com>
2021-01-09 08:40:00 -0800

    'commit'

--- routes/vossbots.js
+++ routes/vossbots.js
@@ -13,7 +13,7 @@ router.get('/getmove', (req, res) => {
 
 	if(req.query.fen) {
 		//const depth = req.query.depth || 5;
-		const depth = 2;
+		const depth = 1;
 		var previousPositions = [{fen: req.query.fen}];
 		const depthChess = new Chess();
 		for(var i = 0; i <= depth; i++)
@@ -25,7 +25,6 @@ router.get('/getmove', (req, res) => {
 				moves.forEach((move) => {
 					depthChess.load(previousPosition.fen);
 					depthChess.move(move);
-					console.log(depthChess.ascii());
 					const eval = evaluateBoard(depthChess.board());
 					if(i === depth) {
 						const isNewHigh = previousPositions.some((prev) => {
