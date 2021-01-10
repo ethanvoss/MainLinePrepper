b0d587c5d3d79df218ca5190bb32337b4846131a
ethanvoss <74939378+ethanvoss@users.noreply.github.com>
2021-01-09 19:25:54 -0800

    'commit'

--- routes/vossbots.js
+++ routes/vossbots.js
@@ -8,69 +8,13 @@ router.get('/', (req, res) => {
 });
 
 router.get('/getmove', (req, res) => {
-	//To do: generate all possible moves for a given depth. => eval moves. => return best move
-	//pick best move based on what move ends in the highest low or highest of the worst outcomes
-
+	//takes too long and heroku times out
 	if(req.query.fen) {
 		//const depth = req.query.depth || 5;
-		const depth = 2;
-		const depthChess = new Chess(req.query.fen);
-		const moves = depthChess.moves();
-		const movesWithEval = [];
-		const side = depthChess.turn();
-		moves.forEach((move) => {
-			depthChess.load(req.query.fen);
-			depthChess.move(move);
-			var previousPositions = [{fen: depthChess.fen()}];
-			for(var i = 1; i <= depth; i++) {
-				var newPositions = [];
-				previousPositions.forEach((position) => {
-					depthChess.load(position.fen);
-					const newMoves = depthChess.moves();
-					newMoves.forEach((newMove) => {
-						depthChess.load(position.fen);
-						depthChess.move(newMove);
-						const eval = evaluateBoard(depthChess.board());
-						newPositions.push({fen: depthChess.fen(), eval: eval});
-					})
-				})
-				previousPositions = newPositions;
-			}
-			var sideMult = 1;
-			if(side === 'b') sideMult *= -1;
-			var worstEval = previousPositions[0];
-			previousPositions.forEach((previousPosition) => {
-				if(previousPosition.eval * sideMult < worstEval.eval * sideMult) worstEval = previousPosition;
-			})
-			console.log(`pushing move ${move}`);
-			movesWithEval.push({move: move, eval: worstEval.eval});
-		})
-		var sideMult = 1;
-		if(side === 'b') sideMult *= -1;
-		var bestMove = movesWithEval[0];
-		movesWithEval.forEach((m) => {
-			if(m.eval * sideMult > bestMove.eval) bestMove = m;
-		})
-		res.send(bestMove.move);
+		
 	} else {
 		res.send('Bee bee boo beep i cant find a move');
 	}
-
-	function evaluateBoard(board) {
-		const values = [{piece: 'k', value: 900},{piece: 'q', value: 90},{piece: 'r', value: 50},{piece: 'b', value: 30},{piece: 'n', value: 30},{piece: 'p', value: 10}];
-		var eval = 0;
-		board.forEach((row) => {
-			row.forEach((piece) => {
-				if(piece !== null) {
-					var pieceValueObj = values.find((value) => { return value.piece === piece.type });
-					var evalAdd = pieceValueObj.value;
-					if(piece.color === 'b') evalAdd *= -1;
-					eval += evalAdd;
-				}
-			})
-		});
-		return eval;
-	}	
 })
 
 function checkAuthenticated(req, res, next) {
